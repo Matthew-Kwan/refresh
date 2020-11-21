0e76b68ba5053817ed6a7b13f932934f8175d3ff
Matthew Kwan <matthew.k913@gmail.com>
2020-11-21 00:41:04 -0500

    Finished Title and Subtitle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React, { useState, useEffect } from 'react' 
+import Quotes from './components/Quotes'
 
 function App() {
+
+  // STATES
+  const [quote, setQuote] = useState('Test Quote');
   
+  const changeQuote = () => {
+    setTimeout(() => {
+      setQuote(String(Math.random()))
+    },5000)
+  }
+
   return (
     <div className="App">
-    
-      <h1 className='title'> Refresh </h1>
 
-      <Quotes/>
-      <Posts/> 
-      <Resources/>
+      <h1 className='title'> Refresh </h1>
+      <p className='subtitle'>The pit stop your mind needs to refresh.</p>
+      <Quotes quote={quote}/> 
       
     </div>
   );
---
+++ src/components/Quotes.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+
+
+const Quotes = ({quote}) => {
+
+    return (
+        <div>
+            <p className='quote'> {quote} </p>
+        </div>
+    )
+}
+
+export default Quotes;
